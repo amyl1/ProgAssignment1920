731f14f54c5d8ef8a096220e4b2bf9723abd2dff
AmyL <51883238+amyl1@users.noreply.github.com>
2019-12-13 16:33:20 +0000

    added planet radius scaling

--- assignment.js
+++ assignment.js
@@ -1,22 +1,23 @@
 var planRadii = [];
 var planDens = [];
-var count =0;
+var orbRadii = [];
+var count = 0;
+var sunRadii = 66;
 d3.csv("https://raw.githubusercontent.com/amyl1/ProgAssignment1920/master/data.csv", function(data) {
   data.forEach(function (d){
   planRadii.push(d.Radius);
+  orbRadii.push(d.Orbradius);
   planDens.push(d.pl_dens);
-  return {planRadii, planDens};
+  return {planRadii, planDens, orbRadii};
   });
-  planRadii.forEach(function(item){
+  orbRadii.forEach(function(item){
       var trans=item/Math.pow(2,0.5);
-      console.log(trans);
       // Planet
       svg.append("circle")
       .attr("class", "planet")
-      .attr("r", radii.planet)
-      .attr("transform", "translate("+ trans*1000+"," + trans*1000+ ")")
+      .attr("r", planRadii[count]*10)
+      .attr("transform", "translate("+ (sunRadii+(trans*1000))+"," + (sunRadii+(trans*1000))+ ")")
       .style("fill", "rgba(0, 0, "+planDens[count]*100+",1.0)");
-      console.log(planDens[count]);
     count=count+1;
     });
 });
@@ -27,11 +28,7 @@ var width = 1300,
     height = 1000,
     radius = Math.min(width, height);
  
-var radii = {
-  "sun": radius / 15,
-  "planetOrbit": radius / 2.5,
-  "planet": radius / 32,
-};
+
  
 // Space
 var svg = spacetime.append("svg")
@@ -43,7 +40,7 @@ var svg = spacetime.append("svg")
 // Sun
 svg.append("circle")
   .attr("class", "sun")
-  .attr("r", radii.sun)
+  .attr("r", sunRadii)
   .style("fill", "rgba(255, 204, 0, 1.0)");
 
 
