ea42e4ff9222527247c984076bdacd466567533f
AmyL <51883238+amyl1@users.noreply.github.com>
2020-01-12 17:27:58 +0000

    added event listner

--- assignment.js
+++ assignment.js
@@ -1,64 +1,19 @@
-var planName=[];
-var planRadii = [];
-var planDens = [];
-var orbRadii = [];
-var count = 0;
 var sunRadii = 10;
 var width = 1000;
 var height = 600;
+var dataset=[];
+var radius=10;
 d3.csv("https://raw.githubusercontent.com/amyl1/ProgAssignment1920/master/data.csv", function(data) {
-  data.forEach(function (d){
-  planName.push(d.pl_Name);
-  planRadii.push(d.Radius);
-  orbRadii.push(d.Orbradius);
-  planDens.push(d.pl_dens);
-  return {planRadii, planDens, orbRadii};
-  });
-  orbRadii.forEach(function populate(item){
-    var angle=Math.random()*360;
-    if (angle<90){
-      var transx=((Math.sin(angle)*(item*5000))+100)
-      var transy=((Math.cos(angle)*(item*5000))+100)
-    }
-    else if (angle<180){
-      var transx=-((Math.sin(angle)*(item*5000))+100)
-      var transy=((Math.cos(angle)*(item*5000))+100)
-    }
-    else if (angle<270){
-      var transx=((Math.sin(angle)*(item*5000))+100)
-      var transy=-((Math.cos(angle)*(item*5000))+100)
-    }
-    else{
-      var transx=-((Math.sin(angle)*(item*5000))+100)
-      var transy=-((Math.cos(angle)*(item*5000))+100)
-    }
-      // Planet
-
-      svg.append("circle")
-      .attr("class", "planet")
-      .attr("cx",100)
-      .attr("cy",75)
-      .attr("r", planRadii[count]*5)
-      .transition()
-      .attr("transform", "translate("+transx+","+transy+")")
-      .attr("stroke","white")
-      .attr("stroke-width", 0.5)
-      .style("fill", "rgba("+planDens[count]*10+","+planDens[count]*100+","+planDens[count]*1000+",1)");
-      count=count+1;
-    });
-    
-});
-
+   
 var spacetime = d3.select('body');
-
-// Space
+//Get SVG ready
 var svg = spacetime.append("svg")
   .attr("width", width)
   .attr("height", height)
   .append("g")
   .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
  
-// Sun
+// Add sun
 svg.append("circle")
   .attr("class", "sun")
   .attr("r", sunRadii)
@@ -67,3 +22,47 @@ svg.append("circle")
   .attr("stroke","white")
   .attr("stroke-width", 0.5)
   .style("fill", "rgba(255, 204, 0, 1.0)");
+    things = []
+          //Populate things (your "dataset" variable that I didn't see) with properties of each planet
+
+  data.forEach(function populate(d){
+      //For each planet, work out properties and add to the list of planets
+    var angle=Math.random()*360;
+    if (angle<90){
+      var transx=((Math.sin(angle)*(d.Orbradius*5000))+100)
+      var transy=((Math.cos(angle)*(d.Orbradius*5000))+100)
+    }
+    else if (angle<180){
+      var transx=-((Math.sin(angle)*(d.Orbradius*5000))+100)
+      var transy=((Math.cos(angle)*(d.Orbradius*5000))+100)
+    }
+    else if (angle<270){
+      var transx=((Math.sin(angle)*(d.Orbradius*5000))+100)
+      var transy=-((Math.cos(angle)*(d.Orbradius*5000))+100)
+    }
+    else{
+      var transx=-((Math.sin(angle)*(d.Orbradius*5000))+100)
+      var transy=-((Math.cos(angle)*(d.Orbradius*5000))+100)
+    }
+    d.transx = transx;
+    d.transy = transy;
+
+    dataset.push(d);
+});
+      // Add planets to svg
+      svg.selectAll("circle")
+      .data(dataset)//This is a list of circle objects to make
+      .enter()
+      .append("circle")
+      .attr("cx",100)
+      .attr("cy",75)
+      .attr("r", function (d) { return (d.Radius)*5;})
+      .transition()
+      .attr("transform", function(d) { return "translate("+d.transx+","+d.transy+")"})
+      .attr("stroke","white")
+      .attr("stroke-width", 0.5)
+      .style("fill", function(d){return "rgba(0,"+(d.pl_dens*100)+","+(d.pl_dens*100)+",1)";});
+      svg.on("click",function(){
+        alert("Hello");
+      })
+    });
---
+++ test2.js
@@ -0,0 +1,4 @@
+var dataset=[];
+d3.csv("https://raw.githubusercontent.com/amyl1/ProgAssignment1920/master/data.csv", function(data) {
+  console.log(data);
+});
