9f13d27e89b439a8f8e54efb43aac38e00c0487f
AmyL <51883238+amyl1@users.noreply.github.com>
2020-01-15 09:41:52 +0000

    Added jsdoc comments

--- assignment.js
+++ assignment.js
@@ -2,27 +2,32 @@
 var width = screen.width;
 /** Sets the height of the visulisation to the brower screen height. */
 var height = screen.height;
-
-  /** Sets up the visualisation 
-   * @param data data from the CSV fle, which is held on github
+/**
+  * @function loadData
+  * @description loads the data from the csv file which is held on github
+  * @param data the data held within the csv file
 */
-function setup(){
-  d3.csv("https://raw.githubusercontent.com/amyl1/ProgAssignment1920/master/data.csv", function loadData(data) {
-  /** Sets up d3 */ 
+d3.csv("https://raw.githubusercontent.com/amyl1/ProgAssignment1920/master/data.csv", function loadData(data) {
+/** @member spacetime 
+ * @description sets up d3*/  
   var spacetime = d3.select('body');
 
-  /**Defines the div for the tooltip */ 
+  /**@member div
+   * @description Defines the div for the tooltip */ 
   var div = d3.select("body").append("div")	
       .attr("class", "tooltip")				
       .style("opacity", 0);
-   /** Sets up SVG */
+
+   /**@member svg
+    * @description sets up svg canvas.
+    */
   var svg = spacetime.append("svg")
     .attr("width", width)
     .attr("height", height)
     .append("g")
     .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
   
-  /**Introduces the sun element */
+  //Introduces the sun element
   svg.append("circle")
     .attr("class", "sun")
     .attr("r", 10)
@@ -31,13 +36,16 @@ function setup(){
     .attr("stroke","white")
     .attr("stroke-width", 0.5)
     .style("fill", "rgba(255, 204, 0, 1.0)");
-    /** Contains the data used for the visulisation.  */
+     /**@member {array} dataset
+    * @description holds the data from the csv file in an array.
+    */
     var dataset=[];
-    var names=[];
     /** Populates dataset with data for each item in the CSV */
 
-    data.forEach(function GenerateData(d){
-    /** For each planet, work out the translation values and name. Adds them to the list of planets */
+    data.forEach(function GenTransform(d){
+    /** @function GenTransform
+     * @description For each planet, work out the translation values, designed to randomly distribute the planets around the sun. The distance from the centre of the sun in this visualisation is to scale with the distace of the exoplanet from their nearest sun. Pushes each data item to the dataset array. 
+     * @param d data from the csv file*/
       var angle=Math.random()*360;
       if (angle<90){
         var transx=((Math.sin(angle)*(d.Orbradius*5000))+100)
@@ -58,10 +66,9 @@ function setup(){
       d.transx = transx;
       d.transy = transy;          
       dataset.push(d);
-      names.push(d.pl_name);
-      return (names);
   });
-  /**Contains the attributes for the planets */
+  /** @member circleAttr
+   * @description contains the attributes for the planets. Radius is to scale, based on the actual radius of the exoplanet, compared to the radius of the Earth. */
   var circleAttr={
     cx:100,
     cy:75,
@@ -76,10 +83,26 @@ function setup(){
         .enter()
         .append("circle")
         .attr(circleAttr)
-        //.transition()
-        .attr("transform", function Transform(d) { return "translate("+d.transx+","+d.transy+")"})
-        .attr("fill", function gen_Colour(d){return "rgba(0,"+(d.pl_dens*100)+","+(d.pl_dens*100)+",1)";})
-        .on("mouseover", function(d) {		
+        .transition()
+        /**
+         * @function Transform
+         * @param d The data from the csv
+         * @description Transforms the planets from the sun, based on the traslation variables calcualted in the GenTransform function.
+         */
+        .attr("transform", function Transform(d) 
+        { return "translate("+d.transx+","+d.transy+")"})
+        /**
+         * @function genColour
+         * @param d The data from the csv
+         * @description Sets the colour of the planet based on its density. The darker the colour in the visualisation, the more dense the planet is.
+         */
+        .attr("fill", function genColour(d){return "rgba(0,"+(d.pl_dens*100)+","+(d.pl_dens*100)+",1)";})
+                /**
+         * @function showTooltip
+         * @param d The data from the csv
+         * @description Creates a tooltip, containing the name of the planet that the circle represents, when the mouse is hovered over the object.
+         */
+        .on("mouseover", function showTooltip(d) {		
           div.transition()		
                 .duration(200)		
                 .style("opacity", .9);		
@@ -87,13 +110,15 @@ function setup(){
                 .style("left", (d3.event.pageX) + "px")		
                 .style("top", (d3.event.pageY - 28) + "px");
         })
-          			
-      .on("mouseout", function(d) {	
+              /**
+         * @function hideTooltip
+         * @description Removes the description when the mouse is moved from over the planet.
+         */    			
+      .on("mouseout", function hideTooltip() {	
         div.transition()		
                 .duration(500)		
                 .style("opacity", 0);		
       })
 
       });
-    }
-   setup() 
+ 
